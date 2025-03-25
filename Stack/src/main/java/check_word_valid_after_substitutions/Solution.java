ff048bc332a10c6c1233770c599a9fb683d0f26c
Isha Jain <isha.jain0806@gmail.com>
2025-03-26 01:27:56 +0530

    DSA-000: Check word valid after substitutions
    DSA-000: Check word valid after substitutions

---
+++ Stack/src/main/java/check_word_valid_after_substitutions/Solution.java
@@ -0,0 +1,42 @@
+package check_word_valid_after_substitutions;
+
+import java.util.*;
+
+public class Solution {
+
+    public boolean isValidWordAfterSubstitutions(String s) {
+        Stack<Character> stk = new Stack();
+
+        for(int i=0;i<s.length();i++){
+
+            char ch = s.charAt(i);
+
+            if(ch == 'a' || ch == 'b'){
+                stk.push(ch);
+            }
+            else{
+
+                if(stk.isEmpty()){
+                    return false;
+                }
+
+                if(stk.peek() != 'b'){
+                    return false;
+                }
+                stk.pop();
+
+                if(stk.isEmpty()){
+                    return false;
+                }
+
+                if(stk.peek() != 'a'){
+                    return false;
+                }
+                stk.pop();
+
+            }
+        }
+
+        return stk.isEmpty();
+    }
+}
